8724c1c3373f6348a64bf39557b6402befd79d67
mdapathy <md.apathy@gmail.com>
2020-10-06 16:46:50 +0300

    Add repository spring layout

---
+++ src/test/java/com/ip737/transportcompany/transportcompany/TransportCompanyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ip737.transportcompany.transportcompany;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportCompanyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
