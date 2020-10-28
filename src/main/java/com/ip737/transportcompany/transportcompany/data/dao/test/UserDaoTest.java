88881de12c795f614997f11edde35f5949a20031
vladShadow <vladivashchuk@gmail.com>
2020-10-29 01:03:24 +0200

    testiki started

---
+++ src/main/java/com/ip737/transportcompany/transportcompany/data/dao/test/UserDaoTest.java
@@ -0,0 +1,28 @@
+package com.ip737.transportcompany.transportcompany.data.dao.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserDaoTest {
+
+    @Test
+    void getByEmail() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void getByActivationUrl() {
+    }
+
+    @Test
+    void getByRecoverUrl() {
+    }
+}
---
+++ src/main/java/com/ip737/transportcompany/transportcompany/services/test/UserServiceTest.java
@@ -0,0 +1,26 @@
+package com.ip737.transportcompany.transportcompany.services.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserServiceTest {
+
+    @org.junit.jupiter.api.Test
+    void save() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getByActivationUrl() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getByRecoverUrl() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getByEmail() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void update() {
+    }
+}
