ebce43ae530425d8a6560d7d0028c468f661a592
Artem Greco <otschebutsch@gmail..com>
2020-10-29 14:00:33 +0200

    Okay, let's roll

---
+++ src/test/java/com/ip737/transportcompany/transportcompany/services/test/EmailServiceTest.java
@@ -0,0 +1,20 @@
+package com.ip737.transportcompany.transportcompany.services.test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmailServiceTest {
+
+    @Test
+    void createRecoverMail() {
+    }
+
+    @Test
+    void createBasicRegMail() {
+    }
+
+    @Test
+    void sendMailMessage() {
+    }
+}
