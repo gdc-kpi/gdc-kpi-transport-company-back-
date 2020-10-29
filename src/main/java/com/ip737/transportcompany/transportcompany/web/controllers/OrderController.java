45f42db3f6a94395ad87b0002cc8abc56baaac0d
LishchukI <lishchukis98@gmail.com>
2020-10-29 19:38:22 +0200

    second commit

--- src/main/java/com/ip737/transportcompany/transportcompany/web/controllers/OrderController.java
+++ src/main/java/com/ip737/transportcompany/transportcompany/web/controllers/OrderController.java
@@ -2,18 +2,16 @@ package com.ip737.transportcompany.transportcompany.web.controllers;
 
 import com.ip737.transportcompany.transportcompany.request.InsertOrderRequest;
 import com.ip737.transportcompany.transportcompany.services.OrderService;
-import com.ip737.transportcompany.transportcompany.services.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-
 @Slf4j
 @RestController
 @CrossOrigin
-@RequestMapping("/profile")
+@RequestMapping("/order")
 public class OrderController {
 
     final private OrderService orderService;
@@ -23,14 +21,9 @@ public class OrderController {
         this.orderService = orderService;
     }
 
-    @PostMapping("/insert-vehicle")
+    @PostMapping("/insert-order")
     public ResponseEntity<?> addVehicle(@RequestBody InsertOrderRequest order) {
         orderService.insertOrder(order.toOrder());
-        return new ResponseEntity<>(HttpStatus.OK);
-    }
-
-    @GetMapping()
-    public ResponseEntity<?> profile() {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
