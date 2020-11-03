e406691681f9780636d3a67c2a44b4b071eee959
vladShadow <vladivashchuk@gmail.com>
2020-11-03 19:27:22 +0200

    gosh dat works

--- src/test/java/com/ip737/transportcompany/transportcompany/TransportCompanyApplicationTest.java
+++ src/test/java/com/ip737/transportcompany/transportcompany/TransportCompanyApplicationTest.java
@@ -1,14 +1,24 @@
 package com.ip737.transportcompany.transportcompany;
 
+import com.ip737.transportcompany.transportcompany.web.controllers.AuthController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class TransportCompanyApplicationTest {
 
+    @Autowired
+    private AuthController controller;
+
     @Test
-    public void contextLoads() {
+    public void contextLoads() throws Exception {
+        assertThat(controller).isNotNull();
     }
 }
