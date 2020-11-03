111177eebbffc688de59d2249616268ad50f957b
vladShadow <vladivashchuk@gmail.com>
2020-11-04 01:38:03 +0200

    test 1 done

--- src/test/java/com/ip737/transportcompany/transportcompany/data/dao/impl/UserDaoImplTest.java
+++ src/test/java/com/ip737/transportcompany/transportcompany/data/dao/impl/UserDaoImplTest.java
@@ -3,10 +3,13 @@ package com.ip737.transportcompany.transportcompany.data.dao.impl;
 import com.ip737.transportcompany.transportcompany.configs.constants.Constants;
 import com.ip737.transportcompany.transportcompany.data.dao.UserDao;
 import com.ip737.transportcompany.transportcompany.data.entities.User;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.hamcrest.Matchers.samePropertyValuesAs;
 import static org.junit.Assert.assertEquals;
 
 @SpringBootTest
@@ -14,6 +17,16 @@ class UserDaoImplTest {
 
     @Autowired
     private UserDao userDao;
+
+    @Before
+    void setUp() {
+        userDao.delete("notamongass@gmail.com", "password");
+    }
+
+    @After
+    void tearDown() {
+        userDao.delete("notamongass@gmail.com", "password");
+    }
 
     @Test
     void getByActivationUrl() {
@@ -40,8 +53,8 @@ class UserDaoImplTest {
                 .build();
         userDao.save(user, Constants.ROLE_DRIVER_ID);
         User user2 = userDao.getByEmail(user.getEmail());
-        assertEquals(user2.getEmail(), "notamongass@gmail.com");
-        userDao.delete(user.getEmail(), user.getPassword());
+        user.setId(user2.getId());
+        assertEquals(user, user2);
     }
 
     @Test
